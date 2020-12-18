b3e91f603925b1e731a4be10c0992c1b22c6a6b1
Aleksei <3900676@gmail.com>
2020-12-18 22:36:37 +0300

    first step

---
+++ src/test/java/BaseGithubTest.java
@@ -0,0 +1,56 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.conditions.Visible;
+import config.ConfigHelper;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class BaseGithubTest {
+    private final static String Login = ConfigHelper.getLogin();
+    private final static String Password = ConfigHelper.getPassword();
+
+    private final static String Repository = "qa_hm_3";
+    private final static String Issues_Title = "Homework";
+    private final static String Issues_Comment = "Write a test for creating an Issue in the repository through the Web interface.";
+
+
+    @Test
+    void baseGithubTest() {
+        open("https://github.com");
+        $("[href='/login']").click();
+        System.out.println(Login);
+        $("#login_field").val(Login);
+        $("#password").val(Password);
+        $("[value=\"Sign in\"]").click();
+        $("body").shouldHave(text(Login));
+        $("[aria-label=\"View profile and more\"]").click();
+        $(byText("Your repositories")).click();
+        $(byText(Repository)).click();
+        $("[data-content=\"Issues\"]").click();
+        sleep(250);  // я хз но без этой паузы он не видит кнопку "New issue", был бы рад обьяснению почему.
+        $(".repository-content .btn-primary").shouldBe(visible).click();
+        $("#issue_title").val(Issues_Title);
+        $("#issue_body").val(Issues_Comment);
+        $(byText("Assignees")).click();
+        $(".select-menu-list").find(byText(Login)).click();
+        $(".select-menu-item").sendKeys(Keys.ESCAPE);
+        $(byText("Labels")).click();
+        $(byText("help wanted")).click();
+        $(byText("question")).click();
+        $("[role=\"menuitemcheckbox\"]").sendKeys(Keys.ESCAPE);
+        $$("button[type=\"submit\"]").find(text("Submit new issue")).click();
+        $(".gh-header-show").shouldHave(text(Issues_Title));
+
+
+        $(byText("Delete issue")).click();
+        $(byText("Delete this issue")).click();
+        $(".repository-content").shouldNotHave(text(Issues_Title));
+
+
+
+        //System.out.println();
+    }
+}
